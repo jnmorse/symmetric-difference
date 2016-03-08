a87f63cb36c400d6f4a3455df5af20c2b2bffa94
Joseph Morse <tamed.lionheart@gmail.com>
2016-03-08 12:15:05 -0500

    Simplified Second Filter

--- src/index.js
+++ src/index.js
@@ -2,19 +2,13 @@ module.exports = function sym() {
   var arrays = Array.prototype.slice.call(arguments);
   var result = [];
 
-  result = arrays.reduce(function(a, b) {
-    var temp = a.concat(b).filter(function(value, index, arr) {
+  result = arrays.reduce(function(previous, current) {
+    var temp = previous.concat(current).filter(function(value, index, arr) {
       return arr.indexOf(value) === index;
     });
 
     return temp.filter(function(value) {
-      if (a.indexOf(value) === -1 || b.indexOf(value) === -1) {
-        return true;
-      }
-
-      else {
-        return false;
-      }
+      return previous.indexOf(value) === -1 || current.indexOf(value) === -1;
     });
   }, []);
 
